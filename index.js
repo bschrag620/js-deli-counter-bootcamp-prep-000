3e80278a7d9b4301cf6766147cb55f115098ff45
Brad Schrag <brad.schrag@gmail.com>
2018-07-02 16:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function nowServing(katzDeliLine) {
 	  return 'There is nobody waiting to be served!'
 	}
 }
+
+function currentLine(katzDeliLine) {
+  
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'    
+  } else {
+    var returnString 'The line is currently: 1. ' + katzDeliLine[0]
+    for (var i = 1; i < katzDeliLine.length; i++) {
+      returnString += ', '
+      returnString += i+1
+      returnString += katzDeliLine[i]
+    }
+  }
+  return returnString
+}
